5b19ec24db3eb9d52bf1b614873ec549102cd337
Dharmil Shah <dharmilshah476@gmail.com>
2022-01-26 18:45:45 +0530

    Completed The Responsiveness Of The Portfolio

--- index.js
+++ index.js
@@ -2,14 +2,28 @@ const openMenuBtn = document.querySelector(`#open-menu-btn`);
 const closeMenuBtn = document.querySelector(`#close-menu-btn`);
 const menu = document.querySelector(`.nav-menu`);
 
-openMenuBtn.addEventListener(`click`, ()=> {
+openMenuBtn.addEventListener(`click`, () => {
     menu.style.display = `block`;
     closeMenuBtn.style.display = `block`;
     openMenuBtn.style.display = `none`;
 })
 
-closeMenuBtn.addEventListener(`click`, ()=> {
+const closeMenu = () => {
     menu.style.display = `none`;
     closeMenuBtn.style.display = `none`;
     openMenuBtn.style.display = `block`;
+}
+
+closeMenuBtn.addEventListener(`click`, closeMenu)
+
+if (window.innerWidth < 1024) {
+    document.querySelectorAll(`.nav-menu li a`).forEach(navItem => {
+        navItem.addEventListener(`click`, closeMenu);
+    })
+}
+
+// Changing the nav background on scroll
+
+window.addEventListener(`scroll`, () => {
+    document.querySelector(`nav`).classList.toggle(`window-scroll`, window.scrollY > 0);
 })
